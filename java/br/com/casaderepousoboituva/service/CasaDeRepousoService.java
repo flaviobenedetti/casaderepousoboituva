d1aad0903be29557f46cbc99f073f5678e86c376
Flavio Andre Benedetti <maninhofab@gmail.com>
2018-10-22 20:53:16 -0200

    producao

---
+++ java/br/com/casaderepousoboituva/service/CasaDeRepousoService.java
@@ -0,0 +1,29 @@
+package br.com.casaderepousoboituva.service;
+
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+
+import com.br.casaderepousoboituva.util.JavaMailApp;
+import com.google.gson.Gson;
+
+import br.com.casaderepousoboituva.domain.Contato;
+
+@Path("contato")
+public class CasaDeRepousoService {
+
+	// http://localhost:8080/casaderepousoboituva/rest/contato
+	@POST
+	public String recebeContato(String json){
+		Gson gson = new Gson();
+		Contato contato = gson.fromJson(json, Contato.class);
+
+		JavaMailApp javaMailApp = new JavaMailApp();
+		
+		javaMailApp.EnviarEmail(contato);		
+		
+		String jsonSaida = gson.toJson(contato);
+		return jsonSaida;
+		
+	}
+	
+}
---
+++ java/com/br/casaderepousoboituva/util/RecourceConfig.java
@@ -0,0 +1,13 @@
+package com.br.casaderepousoboituva.util;
+
+import javax.ws.rs.ApplicationPath;
+
+import org.glassfish.jersey.server.ResourceConfig;
+
+//http://localhost:8080/casaderepousoboituva/rest
+@ApplicationPath("rest")
+public class RecourceConfig extends ResourceConfig {
+	public void ResourceConfig(){
+		packages("br.com.casaderepousoboituva.service");
+	}		
+}
